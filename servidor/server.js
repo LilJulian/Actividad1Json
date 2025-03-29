6bb4113cc711a66939e44b9f8edb8c56c8d39f46
LilJulian <juliadar267@gmail.com>
2025-03-28 21:11:09 -0500

    diferente base de datos

--- servidor/server.js
+++ servidor/server.js
@@ -5,6 +5,19 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(
+  jsonServer.rewriter({
+    "/futbolistas": "/players",
+    "/equipos": "/teams",
+    "/ligas": "/leagues",
+    "/estadios": "/stadiums",
+    "/futbolistas/:id": "/players/:id",
+    "/equipos/:id": "/teams/:id",
+    "/ligas/:id": "/leagues/:id",
+    "/estadios/:id": "/stadiums/:id"
+  })
+);
+
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
